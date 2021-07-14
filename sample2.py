80c12eddd01ac94d3dc33fba2fadebdfe487d04d
Guilherme Balduino Lopes <guilhermebalopes@ufu.br>
2021-07-14 01:26:23 -0300

    updates

--- sample2.py
+++ sample2.py
@@ -4,10 +4,10 @@ from threading import *
 
 class MyThread(Thread):
     def run(self):
-        for i in range(20):
+        for i in range(10):
             print("This is a child class")
 
 t = MyThread()
 t.start()
-for i in range(20):
+for i in range(10):
     print("This is the main thread")
--- sample3.py
+++ sample3.py
@@ -4,10 +4,10 @@ from threading import *
 
 class Demo:
     def show(self):
-        for i in range(20):
+        for i in range(10):
             print("This is the child class")
 obj=Demo()
 t=Thread(target=obj.show())
 t.start()
-for i in range(20):
+for i in range(10):
     print("This is the parent thread")
